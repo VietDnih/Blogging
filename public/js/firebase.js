3df105007da63167ee718890e6d8296967d65f1a
xuanviet73 <dinhxuanviet12dll@gmail.com>
2024-09-29 15:26:41 +0700

    Blog
    finished project

---
+++ public/js/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDajIvQziJCpHS-bWorxyVymfWAQtF40r8",
+  authDomain: "blogging-website-a5089.firebaseapp.com",
+  projectId: "blogging-website-a5089",
+  storageBucket: "blogging-website-a5089.appspot.com",
+  messagingSenderId: "217608391850",
+  appId: "1:217608391850:web:7354f0f921ca88023514d3"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
